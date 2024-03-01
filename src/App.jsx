50f59c905872c9159c703970fc4f35858ce1ab76
FerrMat3D <ferrmatstudio@gmail.com>
2024-03-01 09:04:03 -0300

    Teste

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+<div className="h-[200px] w-screen w-100 bg-blue-500">
+
+<div> 
+
+<h1> TESTE </h1>
+
+</div>
+
+</div>
+      
+    </>
+  )
+}
+
+export default App
