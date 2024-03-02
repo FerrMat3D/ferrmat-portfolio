8f6204c0e3bee75a0bfed16f529ece9cddaf14e5
FerrMat3D <ferrmatstudio@gmail.com>
2024-03-02 18:18:50 -0300

    adicionada animacao com framermotion

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import { Header } from './components';
 import ListaProjetos from './components/ListaProjetos';
-
+import { motion, useAnimation } from "framer-motion";
+import { useEffect } from "react";
 
 export default function App() {
+
+
+  const controls = useAnimation();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      controls.start({
+        y: -scrollY,
+        transition: { type: 'spring', stiffness: 500, damping: 200 }
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [controls]);
+
 
   return (
     <div className='bg-gray-100 py-8 quantico-regular'>
@@ -17,14 +36,16 @@ export default function App() {
 
 </div>
 
-  <div className="mr-4 w-[80%]">
+  <motion.div className="mr-4 w-[80%]"
+  style={{ position: "relative" }} // necessário para animação de rolagem suave
+  animate={controls}>
 
 
 
   <ListaProjetos/>
 
   
-  </div>
+  </motion.div>
 
 
 </div>
