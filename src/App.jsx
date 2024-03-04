26202f3b4425200c93074c8ba282b9ea91928ba4
FerrMat3D <ferrmatstudio@gmail.com>
2024-03-04 12:55:36 -0300

    Novos efeitos Adicionados

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Header } from './components';
 import ListaProjetos from './components/ListaProjetos';
 import { motion, useAnimation } from "framer-motion";
-import { useEffect, useRef } from "react";
-import { Canvas } from '@react-three/fiber';
-import { Box, OrbitControls } from '@react-three/drei';
+import { useEffect, useState  } from "react";
+import { Canvas  } from '@react-three/fiber';
+import { Box, PerspectiveCamera } from '@react-three/drei';
 import { GreekStatue } from './GLTFComponents/GreekStatue';
 
 
@@ -17,7 +17,13 @@ export default function App() {
 
 
   const controls = useAnimation();
-  const orbitControls = useRef(); // Referência para os controles de órbita
+
+  const controlsleft = useAnimation();
+ // const orbitControls = useRef(); // Referência para os controles de órbita
+
+ const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+ 
+
 
   useEffect(() => {
     const handleScroll = () => {
@@ -28,10 +34,34 @@ export default function App() {
       });
     };
 
-    window.addEventListener('scroll', handleScroll);
+    const handleScrollleft = () => {
+      const scrollY = window.scrollY;
+      controlsleft.start({
+        y: scrollY,
+        transition: { type: 'spring', stiffness: 100, damping: 50 }
+      });
+    };
 
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [controls]);
+
+    const handleMouseMove = (event) => {
+      setMousePosition({
+        x: (event.clientX / window.innerWidth) * 2 - 1,
+        y: (event.clientY / window.innerHeight) * 2 - 1
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScrollleft);
+    window.addEventListener('mousemove', handleMouseMove);
+    
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('scroll', handleScrollleft);
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+
+  }, [controls, controlsleft]);
 
 
   return (
@@ -41,16 +71,28 @@ export default function App() {
       </div>
 
 
-      <div className='flex justify-items-end justify-end h-fit '>
-  <div className="w-[50%] bg-gray-100">
+      <div className='flex justify-items-end justify-end '>
+  <motion.div className="w-[80%] bg-gray-100" animate={controlsleft}>
+
+
     <Canvas className='bg-gray-100'>
-      <ambientLight intensity={0.1} />
-      <directionalLight intensity={0.5}/>
-      <GreekStatue position={[50, 0, 0]} rotation={[0, 0, 0]} />
+    <PerspectiveCamera 
+              makeDefault // Defina a câmera como padrão
+              position={[0, 0, 300]} // Defina a posição inicial da câmera
+              fov={75} // Defina o campo de visão
+              aspect={window.innerWidth / window.innerHeight} // Defina a proporção da tela
+              near={0.1} // Defina o plano de corte próximo
+              far={1000} // Defina o plano de corte distante
+            />
+
+
+      <GreekStatue position={[0, -50, -50]} rotation={[(mousePosition.y -0.02 * Math.PI /4) /2 , (mousePosition.x -0.2* Math.PI ) , (mousePosition.y * -Math.PI /5)/4]} />
       <EffectComposer>
+
+
       <Glitch
-    delay={[1.5, 10]} // min and max glitch delay
-    duration={[0.2, 1.0]} // min and max glitch duration
+    delay={[0.1, 1]} // min and max glitch delay
+    duration={[0.1, 0.5]} // min and max glitch duration
     strength={[0.3, 1.0]} // min and max glitch strength
     mode={GlitchMode.SPORADIC} // glitch mode
     active // turn on/off the effect (switches between "mode" prop and GlitchMode.DISABLED)
@@ -58,16 +100,13 @@ export default function App() {
   />
 </EffectComposer>
 
-      <OrbitControls
-      ref={orbitControls}
-      enableDamping
-      dampingFactor={0.1} // Experimente diferentes valores aqui
-      rotateSpeed={0.5}
-    />
+
 
     </Canvas>
-  </div>
-  <motion.div className="mr-4 w-[100%]" style={{ position: "relative", zIndex: 1 }} animate={controls}>
+  </motion.div>
+
+
+  <motion.div className="mr-4 ml-auto w-[125%]" style={{ position: "relative", zIndex: 1 }} animate={controls}>
     <ListaProjetos/>
   </motion.div>
 </div>
